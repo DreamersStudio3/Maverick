da30f2076123942cb4d32cee83ebf359001ecf74
kmklko453-cmyk <kmklko453@gmail.com>
2026-06-15 17:18:03 +0900

    Feat: StateTree 플러그인 및 적 AI 설정 추가

---
+++ Source/Maverick/AI/Task/MVBossLookAtTargetTask.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeTaskBase.h"
+#include "Character/MVCharacterBase.h"
+#include "MVBossLookAtTargetTask.generated.h"
+
+USTRUCT()
+struct FMVBossLookAtTargetTaskData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Input|Target")
+	TObjectPtr<AActor> Target;
+	
+	UPROPERTY(EditAnywhere, Category = "Input|Owner")
+	TObjectPtr<AActor> Owner;
+	
+	UPROPERTY(EditAnywhere, Category = "Input|TurnSpeed")
+	float TurnSpeed = 5.0f;;
+		
+};
+
+USTRUCT()
+struct FMVBossLookAtTargetTask : public FStateTreeTaskCommonBase
+{
+	
+	GENERATED_BODY()
+	
+	using FInstanceData = FMVBossLookAtTargetTaskData;
+	
+	virtual EStateTreeRunStatus EnterState(
+		FStateTreeExecutionContext& Context,
+		const FStateTreeTransitionResult& Transition
+		) const override;
+	
+	virtual EStateTreeRunStatus Tick(
+		FStateTreeExecutionContext& Context,
+		const float DeltaTime
+		) const override;
+	
+	virtual void ExitState(
+		FStateTreeExecutionContext& Context,
+		const FStateTreeTransitionResult& Transition
+		) const override;
+	
+
+public: 
+		
+};
