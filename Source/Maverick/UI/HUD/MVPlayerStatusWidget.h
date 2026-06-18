1773607533174f3c11dac8eaf57f09440150f0d5
곽민규 <mindol0320@gmail.com>
2026-06-18 17:45:33 +0900

    기능 추가: 플레이어 상태 HUD 위젯 연결
    * 기능 추가: WBP 기반 메인 HUD와 플레이어 상태바 에셋 추가
    
    * 기능 추가: HP/MP/Stamina 상태바 크기와 표시 갱신 로직 연결
    
    * 기능 추가: 기본 HUD 클래스를 메인 HUD 위젯으로 설정

--- Source/Maverick/UI/HUD/MVPlayerStatusWidget.h
+++ Source/Maverick/UI/HUD/MVPlayerStatusWidget.h
@@ -6,6 +6,7 @@
 
 class UMVStatComponent;
 class UMVStatusBarWidget;
+class AMVCharacterBase;
 
 UCLASS(Blueprintable)
 class MAVERICK_API UMVPlayerStatusWidget : public UMVWidgetBase
@@ -20,6 +21,7 @@ public:
 	void UnbindStatComponent();
 
 protected:
+	virtual void NativeOnInitialized() override;
 	virtual void NativeConstruct() override;
 	virtual void NativeDestruct() override;
 
@@ -32,7 +34,58 @@ protected:
 	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional), Category = "Maverick|UI|Status")
 	TObjectPtr<UMVStatusBarWidget> MPBar;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "1.0"))
+	float StatusBarReferenceMaxValue = 100.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float HPBaseBarWidth = 150.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float HPMaxBarWidth = 300.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float HPWidthPerMaxPoint = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float HPBarHeight = 11.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float MPBaseBarWidth = 130.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float MPMaxBarWidth = 260.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float MPWidthPerMaxPoint = 0.35f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float MPBarHeight = 8.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float StaminaBaseBarWidth = 110.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float StaminaMaxBarWidth = 220.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float StaminaWidthPerMaxPoint = 0.25f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Maverick|UI|Status|Size", meta = (ClampMin = "0.0"))
+	float StaminaBarHeight = 6.0f;
+
 private:
+	void BuildNativeWidgetTree();
+	void ConfigureStatusBars();
+	void BindToCharacter(AMVCharacterBase* InCharacter);
+	void UnbindCharacter();
+	void ApplyStatusBarSize(
+		UMVStatusBarWidget* StatusBar,
+		float MaxValue,
+		float BaseWidth,
+		float MaxWidth,
+		float WidthPerMaxPoint,
+		float Height) const;
+
 	UFUNCTION()
 	void HandleHPChanged(float CurrentValue, float MaxValue);
 
@@ -42,6 +95,12 @@ private:
 	UFUNCTION()
 	void HandleMPChanged(float CurrentValue, float MaxValue);
 
+	UFUNCTION()
+	void HandleStatRecentLossHoldChanged(bool bInHold);
+
 	UPROPERTY(Transient)
 	TObjectPtr<UMVStatComponent> BoundStatComponent;
+
+	UPROPERTY(Transient)
+	TObjectPtr<AMVCharacterBase> BoundCharacter;
 };
