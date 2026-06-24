7e299467bb90c55bbdf200255976a71f0132be18
kmklko453-cmyk <kmklko453@gmail.com>
2026-06-24 10:20:29 +0900

    Feat: 네임리스 퍼펫 공격 태스크와 무기 연결 추가

---
+++ Source/Maverick/AI/MVEnemyWeapon.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MVEnemyWeapon.generated.h"
+
+UCLASS()
+class MAVERICK_API AMVEnemyWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMVEnemyWeapon();
+	
+	void AttachCombinedToHand(USkeletalMeshComponent* CharacterMesh);
+	void AttachDualToHands(USkeletalMeshComponent* CharacterMesh);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	
+private:	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USkeletalMeshComponent> CombineWeapon;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USkeletalMeshComponent> LeftWeapon;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USkeletalMeshComponent> RightWeapon;
+	
+
+};
